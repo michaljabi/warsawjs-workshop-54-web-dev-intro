4e197c6dc51d66170a8bd604bf83ea3219cf0911
Jabi <michal.jabi@gmail.com>
2021-05-23 12:15:59 +0200

    project: part 3 - react to events

--- project/main.js
+++ project/main.js
@@ -14,15 +14,38 @@ const gameTitle = 'Papier Kamień czy Nożyce ?!';
 const h1element = document.querySelector('h1#game-title');
 h1element.textContent = gameTitle;
 
+// Event listener możesz podpiąć do dowolnego elementu,
+// do którego masz referencje:
+
+// h1element.addEventListener('click', function() {
+//     alert('Hello!')
+// })
+
 /*
     Zadanie 2:
     Upewnij się że selektujesz dodane buttony w HTML:
 */
 const btnPaper = document.getElementById('btn-paper');
-
 const btnRock = document.querySelector('#btn-rock');
 const btnScissors = document.querySelector('#btn-scissors');
 
 console.log(btnPaper, btnRock, btnScissors)
 
+/*
+    Zadanie 3:
+    Podepnij eventListener 'click' do buttonów i wyświet na konsoli
+    odpowiednie wartości w zależności od wyboru: rock, paper, scissors
+*/
+
+btnPaper.addEventListener('click', function() {
+    console.log('paper')
+});
+
+btnRock.addEventListener('click', function() {
+    console.log('rock')
+});
+
+btnScissors.addEventListener('click', function() {
+    console.log('scissors')
+});
 
